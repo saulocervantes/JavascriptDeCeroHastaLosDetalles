b4829e52ee77470c99a1d161d2134b000fe8196e
saulocervantes <saulo.cervantes@hotmail.com>
2017-01-26 00:38:31 -0600

    08 Orden de las importaciones

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,15 +1,3 @@
 
 
-function imprimir(){
-
-    for( var i = 0; i < 800; i++)
-    {
-        console.log( "Imprimio" );
-    }
-}
-
-function presionoClick(){
-    console.log("Click en botón");
-}
-
-imprimir();
+var nombre;
