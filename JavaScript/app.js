0a3d999d474267602d8374c6efd9053a15ad04d0
saulocervantes <saulo.cervantes@hotmail.com>
2017-02-08 13:50:29 -0600

    09 Escritura dinámica - Tipos Primitivos

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,3 +1,17 @@
 
 
-var nombre;
+var num = 10;
+var str = "texto";
+var bol = true;
+var und = undefined;
+var nul = null;
+
+var obj = {
+    numero: 10,
+    texto: "Nuevo texto",
+
+    objHijo: {
+        numero2: 20,
+        text2: "Número texto 2"
+    }
+};
