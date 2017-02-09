a7ebaf8a7f6612b6b7535b1c6c5ca7e0f797cfa6
saulocervantes <saulo.cervantes@hotmail.com>
2017-02-09 17:15:26 -0600

    14 El retorno de las funciones Parte 1

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,11 +1,35 @@
 
 
-function imprimir( fn ){
-    fn();
+function obtenerAleatorio(){
+    return Math.random();
 }
 
-var miFuncion = function(){
-    console.log("miFuncion");
+function obtenerNombre(){
+    return "Juan";
 }
 
-imprimir( miFuncion );
+function esMayor05(){
+
+    if ( obtenerAleatorio() > 0.5 ) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+if( esMayor05() ){
+    console.log("Es mayor a 0.5");
+}else{
+    console.log("Es menor a 0.5");
+}
+
+function crearPersona( nombre, apellido ){
+    return{
+        nombre: nombre,
+        apellido: apellido
+    }
+}
+
+var persona = crearPersona("Maria", "Paz");
+console.log( persona.nombre );
+console.log( persona.apellido );
