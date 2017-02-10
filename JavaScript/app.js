16a4da53d1f2d3e07bd32315c22a234e6fc82b07
saulocervantes <saulo.cervantes@hotmail.com>
2017-02-10 12:00:37 -0600

    18 El señor de los anillos :: The JavaScript Game

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,19 +1,46 @@
 
 
-function Persona(nombre, apellido){
-    
+function Jugador( nombre ){
+
     this.nombre = nombre;
-    this.apellido = apellido;
-    this.edad = 30;
-    this.imprimirPersona = function(){
-        return this.nombre + " " + this.apellido + "(" + this.edad + ")";
+    this.pv = 100;
+    this.sp = 100;
+
+    this.curar = function( jugadorObjetivo ){
+
+        if ( this.sp >= 40 ) {
+            this.sp -= 40;// El jugador que dispare la habilidad perdera "energía"(special points)
+            jugadorObjetivo.pv += 20;
+        }else{
+            console.info( this.nombre + " no tiene sp")
+        }
+
+        this.estado( jugadorObjetivo );
     }
-    
+
+    this.estado = function( jugadorObjetivo ){
+        console.info( this );
+        console.info( jugadorObjetivo );
+    }
+
+    this.tirarFlecha = function( jugadorObjetivo ) {
+
+        if ( jugadorObjetivo.pv > 40 ) {
+            jugadorObjetivo.pv -= 40;
+        }else{
+            jugadorObjetivo.pv = 0;
+            console.error( jugadorObjetivo.nombre + " murio!!" );
+        }
+        this.estado( jugadorObjetivo );
+        
+    }
+
 }
 
-var juan = new Persona("Juan", "Mendoza");
+var gandalf = new Jugador( "Gandalf" );
+var legolas = new Jugador( "Legolas" );
 
-console.log( juan );
-console.log( juan.nombre );
-console.log( juan.imprimirPersona());
+console.log( gandalf );
+console.log( legolas );
 
+gandalf.curar( legolas );
