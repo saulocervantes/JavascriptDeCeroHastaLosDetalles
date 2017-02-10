56c56b12812edea13e3099e3c97e5870cc33154d
saulocervantes <saulo.cervantes@hotmail.com>
2017-02-10 00:45:14 -0600

    16 Metodos y el objeto THIS Parte 2

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -7,13 +7,25 @@ var persona = {
     apellido: "Dubon",
     imprimirNombre: function(){
         console.log( this.nombre + " " + this.apellido );
+    },
+    direccion: {
+        pais: "Costa Rica",
+        obtenerPais: function(){
+            
+            var self = this;
+
+            var nuevaDireccion = function(){
+                console.log( this );
+                console.log("La dirección es en " + self.pais );
+            }
+
+            nuevaDireccion();
+        }
     }
 
 }
 
-persona.nombre = "Andrés";
-
 persona.imprimirNombre();
-
+persona.direccion.obtenerPais();
 
 
