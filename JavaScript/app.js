2283e253c40aba0027d8160a0e65e0398fd10feb
saulocervantes <saulo.cervantes@hotmail.com>
2017-02-10 13:58:35 -0600

    19 Prototipos: prototype Parte 1

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,46 +1,19 @@
 
 
-function Jugador( nombre ){
+function Persona(){
+    this.nombre = "Fernando";
+    this.apellido = "Herrera";
+    this.edad = 30;
+    this.pais = "Costa Rica";
 
-    this.nombre = nombre;
-    this.pv = 100;
-    this.sp = 100;
-
-    this.curar = function( jugadorObjetivo ){
-
-        if ( this.sp >= 40 ) {
-            this.sp -= 40;// El jugador que dispare la habilidad perdera "energía"(special points)
-            jugadorObjetivo.pv += 20;
-        }else{
-            console.info( this.nombre + " no tiene sp")
-        }
-
-        this.estado( jugadorObjetivo );
-    }
-
-    this.estado = function( jugadorObjetivo ){
-        console.info( this );
-        console.info( jugadorObjetivo );
-    }
-
-    this.tirarFlecha = function( jugadorObjetivo ) {
-
-        if ( jugadorObjetivo.pv > 40 ) {
-            jugadorObjetivo.pv -= 40;
-        }else{
-            jugadorObjetivo.pv = 0;
-            console.error( jugadorObjetivo.nombre + " murio!!" );
-        }
-        this.estado( jugadorObjetivo );
-        
-    }
-
+    // this.imprimirInfo = 
 }
 
-var gandalf = new Jugador( "Gandalf" );
-var legolas = new Jugador( "Legolas" );
+Persona.prototype.imprimirInfo = function(){
+        console.log( this.nombre + " " + this.apellido + "(" + this.edad + ")");
+}
 
-console.log( gandalf );
-console.log( legolas );
+var fer = new Persona();
 
-gandalf.curar( legolas );
+console.log( fer );
+console.log( fer.imprimirInfo() );
